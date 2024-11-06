585428e475f3e1ad54d9789bc535a6ff6eee20c0
RaffaelePellegrino <raffaele.pellegrino02@gmail.com>
2024-11-06 15:49:49 +0100

    bug nel quarto esercizio, ho creato un altra variabile "nomesecondario"

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -2,12 +2,14 @@
 
 const name = 'Mario';
 
-
+let nomeSecondario = 'Mario'
 // Dichiara la funzione qui.
-
+function saluto(nome){
+    console.log(`Ciao ${nome}`)
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
-
+// saluto(name);
+saluto(nomeSecondario)
 //Risultato atteso se si passa 'Mario': // ciao Mario
