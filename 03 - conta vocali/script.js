d24166c5ccc2ac3524d1e0425f3f735a98428166
RaffaelePellegrino <raffaele.pellegrino02@gmail.com>
2024-11-06 15:41:28 +0100

    esercizio 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function contaLettere(stringa){
+    let tappo = 0
+    let vocaliTrovate = []
+    for (let i=0; i<stringa.length; i++){
+        if(stringa[i] === "a" || stringa[i] === "e" || stringa[i] === "i" || stringa[i] === "o"  || stringa[i] === "u"  ){
+            tappo += 1
+            vocaliTrovate.push(stringa[i])
+        }
+    }
+    return tappo , vocaliTrovate;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(contaLettere(word))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
