983cfa9355b7a1936e247153bd54dac18030f70a
RaffaelePellegrino <raffaele.pellegrino02@gmail.com>
2024-11-06 18:35:43 +0100

    fix programma vocali con un for innestato

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,20 +4,23 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-function contaLettere(stringa){
+function contaLettere(stringa,vocali){
     let tappo = 0
     let vocaliTrovate = []
     for (let i=0; i<stringa.length; i++){
-        if(stringa[i] === "a" || stringa[i] === "e" || stringa[i] === "i" || stringa[i] === "o"  || stringa[i] === "u"  ){
-            tappo += 1
-            vocaliTrovate.push(stringa[i])
+        for (let j=0; j<vocali.length ; j++) {
+            if(stringa[i] === vocali[j]){
+                tappo += 1
+                vocaliTrovate.push(stringa[i])
+            }
         }
     }
     return tappo , vocaliTrovate;
 }
+const vocaliArray = ["A","a","E","e","I","i","O","o","U","u"]
 
 // Invoca la funzione qui e stampa il risultato in console
 
-console.log(contaLettere(word))
+console.log(contaLettere(word,vocaliArray))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
